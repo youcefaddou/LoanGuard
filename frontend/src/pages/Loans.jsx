2bc571b5113be3d3417ddda63a01df28e3e85158
youcefaddou <youcefaddou@gmail.com>
2025-06-28 20:58:13 +0200

    adjusted container in Loans page

--- frontend/src/pages/Loans.jsx
+++ frontend/src/pages/Loans.jsx
@@ -10,9 +10,9 @@ const Loans = () => {
       {/* Contenu principal */}
       <div className="flex-1 lg:ml-64 flex flex-col min-h-screen">
         <Header />
-        <main className="flex-1">
+        <main className="flex-1 p-1 sm:p-1 lg:p-1">
           {/* Contenu des prêts */}
-          <div className="bg-white rounded-lg shadow-sm border border-gray-200 min-h-full">
+          <div className="bg-white rounded-lg shadow-sm border border-gray-200 h-full">
             <h2 className="text-xl font-semibold p-4">Liste des Prêts</h2>
             {/* Ici vous pouvez ajouter le contenu spécifique aux prêts */}
           </div>
