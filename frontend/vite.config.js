f25cc4669ba89941a82a7a9f525bcfa36119c166
youcefaddou <youcefaddou@gmail.com>
2025-09-01 21:04:07 +0200

    ok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,42 +8,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  
-  // Optimisations build
-  build: {
-    // Tree shaking amélioré
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Sépare les librairies lourdes
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'chart-vendor': ['chart.js', 'react-chartjs-2'],
-          'map-vendor': ['leaflet', 'react-leaflet'],
-        }
-      }
-    },
-    // Compression et minification
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      }
-    },
-    // Taille des chunks
-    chunkSizeWarningLimit: 1000,
-  },
-  
-  // Optimisations dev
   server: {
     proxy: {
+      // Dev proxy used when running `vite` locally. In production the front must call
       '/api': 'http://localhost:4000'
     }
-  },
-  
-  // Préchargement intelligent
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'],
-    exclude: ['@vite/client', '@vite/env']
   }
 })
